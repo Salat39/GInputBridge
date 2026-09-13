ae7668987a93da897c0141909f803432df58cb62
nereklama0 <nereklama@inbox.ru>
2026-09-13 14:33:30 +0500

    notifyPomogatorNightMode

--- app/src/main/java/com/salat/gbinder/NightScreen.kt
+++ app/src/main/java/com/salat/gbinder/NightScreen.kt
@@ -1,5 +1,6 @@
 package com.salat.gbinder
 
+import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
 import android.view.View
@@ -18,12 +19,37 @@ class NightScreen : ComponentActivity() {
         })
     }
 
+    override fun onStart() {
+        super.onStart()
+        notifyPomogatorNightMode(on = true)
+    }
+
+    override fun onStop() {
+        notifyPomogatorNightMode(on = false)
+        super.onStop()
+    }
+
     override fun onDestroy() {
         if (activeRef?.get() === this) activeRef = null
         super.onDestroy()
     }
 
+    private fun notifyPomogatorNightMode(on: Boolean) {
+        runCatching {
+            sendBroadcast(
+                Intent(POMOGATOR_NIGHT_MODE_ACTION).apply {
+                    setPackage(POMOGATOR_PACKAGE)
+                    putExtra(POMOGATOR_NIGHT_MODE_EXTRA, if (on) "ON" else "OFF")
+                }
+            )
+        }
+    }
+
     companion object {
+        private const val POMOGATOR_PACKAGE = "ru.pomogator"
+        private const val POMOGATOR_NIGHT_MODE_ACTION = "ru.pomogator.NIGHT_MODE"
+        private const val POMOGATOR_NIGHT_MODE_EXTRA = "state"
+
         @Volatile
         private var activeRef: WeakReference<NightScreen>? = null
 
